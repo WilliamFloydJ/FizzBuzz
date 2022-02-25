323b65ef85779ab97afa9453b71188d3addd2ef4
William <Floydfamily71@gmail.com>
2022-02-24 20:25:27 -0700

    Initial Commit

---
+++ fizzBuzz.js
@@ -0,0 +1,15 @@
+// Write your solution below this line:
+for(let i = 1; i < 101; i++){
+    if(i % 5===0 && i % 3===0){
+     console.log("fizzBuzz")
+   }
+   else if(i % 5===0){
+     console.log("buzz")
+   }
+   else if(i % 3===0){
+     console.log("fizz")
+   }
+   else{
+     console.log(i)
+   }
+}
